04e1ea817c056ef5faa1579d0eab98adeff1db45
Roger So <roger.so@gmail.com>
2012-03-04 03:45:00 +0800

    Add basic django-cms

--- linux_org_hk/urls.py
+++ linux_org_hk/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -11,9 +12,17 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^comments/', include('django.contrib.comments.urls')),
+    url(r'^openid/', include('django_openid_auth.urls')),
+
+    url(r'^', include('cms.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns = patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+        url(r'', include('django.contrib.staticfiles.urls'))
+    ) + urlpatterns
